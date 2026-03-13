9b087c384678d53bbf661f57a5507280b89a2408
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2026-03-13 12:31:32 -0300

    feat: adiciona models do login, view de cadastro e mensagens de erro no cadastro

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,21 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import UserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Usuário precisa de um email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
@@ -8,6 +23,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .models import User
 
 def login_view(request):
     return render(request, "accounts/pages/login.html")
 
 def cadastro_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+
+        if password != password2:
+            messages.error(request, "As senhas não coincidem.")
+            return redirect("accounts:cadastro")
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Este email já está cadastrado.")
+            return redirect("accounts:cadastro")
+
+        user = User.objects.create_user(
+            email=email,
+            password=password
+        )
+        
+        login(request, user)
+        messages.success(request, "Conta criada com sucesso!")
+        return redirect("landing:index") # tenho que mudar isso depois
+
     return render(request, "accounts/pages/cadastro.html")
